29bcf0b36cde0c017b2b40ca1cca7f54511f2162
fullstack290623 <fullstack290623@gmail.com>
2024-09-12 19:01:11 +0300

    upload

--- imdb/create_db_sql.py
+++ imdb/create_db_sql.py
@@ -1,9 +1,9 @@
 import sqlite3
 import pandas as pd
 
-df = pd.read_csv('imdb_top_1000.csv')
-
 conn = sqlite3.connect('imdb_top_1000.db')
+
+df = pd.read_csv('imdb_top_1000.csv')
 df.to_sql('imdb_movies', conn, if_exists='replace', index=False)
 
 conn.commit()
